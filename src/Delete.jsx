ea886afe7bdb03c046a725ca33ff233a05f271ee
Adnane <adnanegu5@gmail.com>
2024-11-22 13:10:43 +0100

    adding useEffect to delete

--- src/Delete.jsx
+++ src/Delete.jsx
@@ -1,14 +1,17 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
 export default function Delete({deleteFnc}){
     const { id } = useParams();
     const navigate = useNavigate()
 
-
+    useEffect(()=>{
+        deleteFnc(id); 
+        navigate("/list");
+    }, [])
     return(
        <>
-        <button onClick={()=>{deleteFnc(id); navigate("/list")}}>delete</button>
+            Task deleting   
         </>
     )
 }
